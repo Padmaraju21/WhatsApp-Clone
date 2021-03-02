f00b6ce804306d539a882d6fdc227db8991af230
pencil <penciljamnoreply@gmail.com>
2021-03-02 01:20:37 +0000

    db issues

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Sidebar from "./Sidebar";
+import Chat from "./Chat";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Login from "./Login";
+import { useStateValue } from "./StateProvider";
+import UseWindowDimensions from "./UseWindowDimensions";
 
 function App() {
+  const [{ user }, dispatch] = useStateValue();
+  const { width } = UseWindowDimensions();
+  const uid =
+    localStorage.getItem("uid") !== undefined
+      ? localStorage.getItem("uid")
+      : null;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {!user && !uid ? (
+        <Login />
+      ) : (
+        <div className="app__body">
+          <Router>
+            <Sidebar />
+            <Switch>
+              <Route path="/rooms/:roomId">
+                <Chat />
+              </Route>
+              <Route path="/">
+                <Chat />
+              </Route>
+            </Switch>
+          </Router>
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,22 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCJRODI6l10A8jmSUGBBDxqKwjsRR5ijBU",
+    authDomain: "whatsapp-clone-ef693.firebaseapp.com",
+    databaseURL: "https://whatsapp-clone-ef693-default-rtdb.firebaseio.com",
+    projectId: "whatsapp-clone-ef693",
+    storageBucket: "whatsapp-clone-ef693.appspot.com",
+    messagingSenderId: "235563324075",
+    appId: "1:235563324075:web:93c615e9ca49a887cfddee",
+    measurementId: "G-GBLD9PQNVT"
+  };
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider };
+export default db;
