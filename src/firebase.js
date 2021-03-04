b22631edc63d0a3b3658e1de1ae08c1199b78e18
pencil <penciljamnoreply@gmail.com>
2021-03-04 19:52:05 +0000

    400 error fixed

--- src/firebase.js
+++ src/firebase.js
@@ -4,15 +4,15 @@ import "firebase/firestore";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyCJRODI6l10A8jmSUGBBDxqKwjsRR5ijBU",
-    authDomain: "whatsapp-clone-ef693.firebaseapp.com",
-    databaseURL: "https://whatsapp-clone-ef693-default-rtdb.firebaseio.com",
-    projectId: "whatsapp-clone-ef693",
-    storageBucket: "whatsapp-clone-ef693.appspot.com",
-    messagingSenderId: "235563324075",
-    appId: "1:235563324075:web:93c615e9ca49a887cfddee",
-    measurementId: "G-GBLD9PQNVT"
-  };
+  apiKey: "AIzaSyCJRODI6l10A8jmSUGBBDxqKwjsRR5ijBU",
+  authDomain: "whatsapp-clone-ef693.firebaseapp.com",
+  databaseURL: "https://whatsapp-clone-ef693-default-rtdb.firebaseio.com",
+  projectId: "whatsapp-clone-ef693",
+  storageBucket: "whatsapp-clone-ef693.appspot.com",
+  messagingSenderId: "235563324075",
+  appId: "1:235563324075:web:93c615e9ca49a887cfddee",
+  measurementId: "G-GBLD9PQNVT"
+};
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
 const auth = firebase.auth();
